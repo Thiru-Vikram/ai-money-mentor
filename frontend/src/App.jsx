c45b0631769be249a78ea98ee6a656b278b5481e
cavin <cavinxp@gmail.com>
2026-03-25 11:41:03 +0530

    Revert "fire planner"
    This reverts commit 5ee0d5e22a70d223748a9bcab4e22c765697435c.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,3 @@
-import { Routes, Route } from 'react-router-dom'
 import './index.css'
 import Navbar from './components/Navbar'
 import HeroSection from './components/HeroSection'
@@ -8,15 +7,7 @@ import PortfolioXRay from './components/PortfolioXRay'
 import SocialProof from './components/SocialProof'
 import FooterCTA from './components/FooterCTA'
 
-// Feature Pages
-import FIREPlanner from './pages/FIREPlanner'
-import HealthScoreQuiz from './pages/HealthScoreQuiz'
-import TaxWizard from './pages/TaxWizard'
-import PortfolioXRayPage from './pages/PortfolioXRayPage'
-import LifeEventAdvisor from './pages/LifeEventAdvisor'
-import CouplesPlanner from './pages/CouplesPlanner'
-
-function LandingPage() {
+function App() {
   return (
     <div className="min-h-screen overflow-x-hidden">
       <Navbar />
@@ -29,20 +20,6 @@ function LandingPage() {
       </main>
       <FooterCTA />
     </div>
-  )
-}
-
-function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/fire-planner" element={<FIREPlanner />} />
-      <Route path="/health-score" element={<HealthScoreQuiz />} />
-      <Route path="/tax-wizard" element={<TaxWizard />} />
-      <Route path="/portfolio-xray" element={<PortfolioXRayPage />} />
-      <Route path="/life-event" element={<LifeEventAdvisor />} />
-      <Route path="/couples-planner" element={<CouplesPlanner />} />
-    </Routes>
   )
 }
 
